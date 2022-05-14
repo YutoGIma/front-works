ab0cc7fd2b1f947d57c402f366408272980f68ee
GimaYuto <yurihanagima@gmail.com>
2022-05-14 17:41:23 +0900

    レスポンシブ対応

--- src/api/data.js
+++ src/api/data.js
@@ -1,17 +1,32 @@
-import Api from './index'
+import Api from "./index";
 
 class getData {
-    getPregectures(){
-        return Api().get("api/v1/prefectures")
-    }
-    getPopulation(id){
-        return Api().get("api/v1/population/composition/perYear",{
-            params: {
-                prefCode: id,
-                cityCode: "-"
-            }
-        })
-    }
+  /** 都道府県のリストを取得
+   *  @returns /result/prefCode 	都道府県コード
+   *  @returns /result/prefName 	都道府県名
+   */
+  getPregectures() {
+    return Api().get("api/v1/prefectures");
+  }
+  /**
+   * 
+   * @param  id prefCode
+   * @param  prefCode 都道府県コード
+   * @param  cityCode 市区町村コード("-")
+   * @returns /result/boundaryYear 実績値と推計値の区切り年
+   * @returns /result/data/label ラベル
+   * @returns /result/data/data/year 年
+   * @returns /result/data/data/value
+   * @returns 人口
+   */
+  getPopulation(id) {
+    return Api().get("api/v1/population/composition/perYear", {
+      params: {
+        prefCode: id,
+        cityCode: "-",
+      },
+    });
+  }
 }
 
-export default new getData()
+export default new getData();
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,77 +2,132 @@
   <div class="small">
     <h1>都道府県人口比較グラフ</h1>
     <div class="prefecture-con">
-      <div class="prefecture" v-for="prefecture in prefectures" :key="prefecture.prefCode">
-        <input type="checkbox" v-model="prefecture.select" @change="selectPrefecture()"/>
-        <label>{{prefecture.prefName}}</label>
+      <div
+        class="prefecture"
+        v-for="prefecture in prefectures"
+        :key="prefecture.prefCode"
+      >
+        <input
+          type="checkbox"
+          v-model="prefecture.select"
+          @change="selectPrefecture()"
+        />
+        <label>{{ prefecture.prefName }}</label>
       </div>
     </div>
-    <line-chart class="line-chart" :chart-data="datacollection"></line-chart>
+    <line-chart
+      class="line-chart"
+      :chart-data="datacollection"
+      :options="options"
+    ></line-chart>
   </div>
 </template>
 
 <script>
-  import LineChart from './LineChart.js'
-  import dataService from '../api/data'
-  const { decycle } = require('json-cyclic')
+import LineChart from "./LineChart.js";
+import dataService from "../api/data";
+const { decycle } = require("json-cyclic");
 
-  export default {
-    components: {
-      LineChart
-    },
-    data () {
-      return {
-        datacollection: null,
-        data:{
-          labels: [1960,1965,1970,1975,1980,1985,1990,1995,2000,2005,2010,2015,2020,2025,2030,2035,2040,2045],
-          datasets:[]
+export default {
+  components: {
+    LineChart,
+  },
+  data() {
+    return {
+      datacollection: null,
+      data: {
+        labels: [
+          1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010,
+          2015, 2020, 2025, 2030, 2035, 2040, 2045,
+        ],
+        datasets: [],
+      },
+      prefectures: [],
+      options: {
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "年度",
+              },
+            },
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "人口",
+              },
+            },
+          ],
         },
-        prefectures:[],
-      }
-    },
-    mounted () {
-      this.getPrefectures()
-      this.selectPrefecture()
-    },
-    methods: {
-      async selectPrefecture() {
-        this.data.datasets = []
-        for(let prefecture of this.prefectures){
-          if(prefecture.select){
-            let populations = await dataService.getPopulation(prefecture.prefCode)
-            let populationValue = []
-            for(let population of populations.data.result.data[0].data){
-              populationValue.push(population.value)
-            }
-            this.data.datasets.push({
-              label: prefecture.prefName,
-              data: populationValue,
-            })
+      },
+    };
+  },
+  mounted() {
+    this.getPrefectures();
+    this.selectPrefecture();
+  },
+  methods: {
+    async selectPrefecture() {
+      this.data.datasets = [];
+      for (let prefecture of this.prefectures) {
+        if (prefecture.select) {
+          let populations = await dataService.getPopulation(
+            prefecture.prefCode
+          );
+          let populationValue = [];
+          for (let population of populations.data.result.data[0].data) {
+            populationValue.push(population.value);
           }
+          this.data.datasets.push({
+            label: prefecture.prefName,
+            data: populationValue,
+          });
         }
-        this.datacollection = decycle(this.data)
-      },
-      async getPrefectures() {
-        const prefectures = await dataService.getPregectures()
-        this.prefectures = prefectures.data.result
-      },
-    }
-  }
+      }
+      this.datacollection = decycle(this.data);
+    },
+    async getPrefectures() {
+      const prefectures = await dataService.getPregectures();
+      this.prefectures = prefectures.data.result;
+    },
+  },
+};
 </script>
 
 <style>
-h1{
+h1 {
   text-align: center;
 }
-.prefecture-con{
-  display:flex;
+.prefecture-con {
+  display: flex;
   flex-wrap: wrap;
 }
-.prefecture{
-  width:calc(100% / 8);
+.prefecture {
+  width: calc(100% / 8);
 }
-.line-chart{
-  width:50%;
-  margin:0 auto;
+.line-chart {
+  width: 50%;
+  margin: 0 auto;
+}
+@media screen and (max-width: 1080px) {
+  .prefecture {
+    width: calc(100% / 6);
+  }
+  .line-chart {
+    width: 70%;
+    margin: 0 auto;
+  }
+}
+@media screen and (max-width: 840px) {
+  .prefecture {
+    width: calc(100% / 4);
+  }
+  .line-chart {
+    width: 90%;
+    margin: 0 auto;
+  }
 }
 </style>
